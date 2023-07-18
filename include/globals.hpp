416bc8f110f53ae76fe42553588c93289a470640
Adrian Durci <2403010d@student.gla.ac.uk>
2023-07-18 21:01:16 +0100

    Cleanup code.

--- include/globals.hpp
+++ include/globals.hpp
@@ -1,70 +1,37 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
+#include <vector>
 #include "raylib.h"
+#include "playersettings.hpp"
 
 namespace copsNrobbers3D
 {
+    //----------------------------------------------------------------------------------
+    // Constants
+    //----------------------------------------------------------------------------------
 
     const int MAX_PLAYERS = 9;
-    extern int playerCount;
-    extern Camera cameras_splitscreen[];
-    extern RenderTexture screens_splitscreen[];
+    
+    const int screenWidth = 1280;
+    const int screenHeight = 720;
 
-
-     //----------------------------------------------------------------------------------
-    // Shared Variables Definition (global)
     //----------------------------------------------------------------------------------
+    // Global Variables
+    //----------------------------------------------------------------------------------
+
     // Font font = { 0 };
     // Music music = { 0 };
     // int endingStatus = 0;           // 1 - Win, 2 - Lose
     // char *sampleFilename = NULL;    // Required for custom music file
 
-    //----------------------------------------------------------------------------------
-    // Module Variables Definition (local)
-    //----------------------------------------------------------------------------------
-    const int screenWidth = 1280;
-    const int screenHeight = 720;
+    extern int player_count;
+    extern std::vector<PlayerSettings> players_settings;
 
-    //  int playerCount = 2;
-    //  const int MAX_PLAYERS = 9;
-
+    extern Camera cameras_splitscreen[];
+    extern RenderTexture screens_splitscreen[];
     extern int slice_height_splitscreen;
-    //  Camera cameras_splitscreen[MAX_PLAYERS];
-    //  RenderTexture screens_splitscreen[MAX_PLAYERS];
 
 } // namespace copsNrobbers3D
-
-// namespace copsNrobbers3D
-// {
-
-//     const int MAX_PLAYERS = 9;
-//     static int playerCount;
-//     static Camera cameras_splitscreen[copsNrobbers3D::MAX_PLAYERS];
-//     static RenderTexture screens_splitscreen[copsNrobbers3D::MAX_PLAYERS];
-
-
-//      //----------------------------------------------------------------------------------
-//     // Shared Variables Definition (global)
-//     //----------------------------------------------------------------------------------
-//     // Font font = { 0 };
-//     // Music music = { 0 };
-//     // int endingStatus = 0;           // 1 - Win, 2 - Lose
-//     // char *sampleFilename = NULL;    // Required for custom music file
-
-//     //----------------------------------------------------------------------------------
-//     // Module Variables Definition (local)
-//     //----------------------------------------------------------------------------------
-//     const int screenWidth = 1280;
-//     const int screenHeight = 720;
-
-//     //  int playerCount = 2;
-//     //  const int MAX_PLAYERS = 9;
-
-//     static int slice_height_splitscreen = screenHeight/playerCount;
-//     //  Camera cameras_splitscreen[MAX_PLAYERS];
-//     //  RenderTexture screens_splitscreen[MAX_PLAYERS];
-
-// } // namespace copsNrobbers3D
 
 #endif
